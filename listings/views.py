09718afddf498ee0f4c2a467e0190538e5e0df22
Roshan-023 <roshansandilya23@gmail.com>
2023-12-10 22:53:43 +0530

    Lookups changes

--- listings/views.py
+++ listings/views.py
@@ -18,174 +18,96 @@ class ListingView(RetrieveAPIView):
     lookup_field = 'slug'
 
 class SearchView(APIView):
-    permission_classes = (permissions.AllowAny, )
-    serializer_class = ListingSerializer
-    
     def post(self, request, format=None):
         queryset = Listing.objects.order_by('-list_date').filter(is_published=True)
         data = self.request.data
 
-        sale_type = data['sale_type']
-        queryset = queryset.filter(sale_type__iexact=sale_type)
-
-        price = data['price']
-        if price == '$0+':
-            price = 0
-        elif price == '$200,000+':
-            price = 200000
-        elif price == '$400,000+':
-            price = 400000
-        elif price == '$600,000+':
-            price = 600000
-        elif price == '$800,000+':
-            price = 800000
-        elif price == '$1,000,000+':
-            price = 1000000
-        elif price == '$1,200,000+':
-            price = 1200000
-        elif price == '$1,500,000+':
-            price = 1500000
-        elif price == 'Any':
-            price = -1
-        
-        if price != -1:
-            queryset = queryset.filter(price__gte=price)
-        
-        bedrooms = data['bedrooms']
-        if bedrooms == '0+':
-            bedrooms = 0
-        elif bedrooms == '1+':
-            bedrooms = 1
-        elif bedrooms == '2+':
-            bedrooms = 2
-        elif bedrooms == '3+':
-            bedrooms = 3
-        elif bedrooms == '4+':
-            bedrooms = 4
-        elif bedrooms == '5+':
-            bedrooms = 5
-        
-        queryset = queryset.filter(bedrooms__gte=bedrooms)
-
-        home_type = data['home_type']
-        queryset = queryset.filter(home_type__iexact=home_type)
-
-        bathrooms = data['bathrooms']
-        if bathrooms == '0+':
-            bathrooms = 0.0
-        elif bathrooms == '1+':
-            bathrooms = 1.0
-        elif bathrooms == '2+':
-            bathrooms = 2.0
-        elif bathrooms == '3+':
-            bathrooms = 3.0
-        elif bathrooms == '4+':
-            bathrooms = 4.0
-        
-        queryset = queryset.filter(bathrooms__gte=bathrooms)
-
-        sqft = data['sqft']
-        if sqft == '1000+':
-            sqft = 1000
-        elif sqft == '1200+':
-            sqft = 1200
-        elif sqft == '1500+':
-            sqft = 1500
-        elif sqft == '2000+':
-            sqft = 2000
-        elif sqft == 'Any':
-            sqft = 0
-        
-        if sqft != 0:
-            queryset = queryset.filter(sqft__gte=sqft)
-        
-        days_passed = data['days_listed']
-        if days_passed == '1 or less':
-            days_passed = 1
-        elif days_passed == '2 or less':
-            days_passed = 2
-        elif days_passed == '5 or less':
-            days_passed = 5
-        elif days_passed == '10 or less':
-            days_passed = 10
-        elif days_passed == '20 or less':
-            days_passed = 20
-        elif days_passed == 'Any':
-            days_passed = 0
-        
-        for query in queryset:
-            num_days = (datetime.now(timezone.utc) - query.list_date).days
-
-            if days_passed != 0:
-                if num_days > days_passed:
-                    slug=query.slug
-                    queryset = queryset.exclude(slug__iexact=slug)
-        
-        has_photos = data['has_photos']
-        if has_photos == '1+':
-            has_photos = 1
-        elif has_photos == '3+':
-            has_photos = 3
-        elif has_photos == '5+':
-            has_photos = 5
-        elif has_photos == '10+':
-            has_photos = 10
-        elif has_photos == '15+':
-            has_photos = 15
-        
-        for query in queryset:
-            count = 0
-            if query.photo_1:
-                count += 1
-            if query.photo_2:
-                count += 1
-            if query.photo_3:
-                count += 1
-            if query.photo_4:
-                count += 1
-            if query.photo_5:
-                count += 1
-            if query.photo_6:
-                count += 1
-            if query.photo_7:
-                count += 1
-            if query.photo_8:
-                count += 1
-            if query.photo_9:
-                count += 1
-            if query.photo_10:
-                count += 1
-            if query.photo_11:
-                count += 1
-            if query.photo_12:
-                count += 1
-            if query.photo_13:
-                count += 1
-            if query.photo_14:
-                count += 1
-            if query.photo_15:
-                count += 1
-            if query.photo_16:
-                count += 1
-            if query.photo_17:
-                count += 1
-            if query.photo_18:
-                count += 1
-            if query.photo_19:
-                count += 1
-            if query.photo_20:
-                count += 1
-            
-            if count < has_photos:
-                slug = query.slug
-                queryset = queryset.exclude(slug__iexact=slug)
-        
-        open_house = data['open_house']
-        queryset = queryset.filter(open_house__iexact=open_house)
-
-        keywords = data['keywords']
-        queryset = queryset.filter(desc__icontains=keywords)
+        for field, value in data.items():
+            if field == 'sale_type':
+                queryset = queryset.filter(sale_type__iexact=value)
+            elif field == 'price':
+                price = self.convert_price_range(value)
+                if price is not None:
+                    queryset = queryset.filter(price__lte=price)
+            elif field == 'bedrooms':
+                bedrooms = int(value.rstrip('+'))
+                queryset = queryset.filter(bedrooms__gte=bedrooms)
+            elif field == 'home_type':
+                queryset = queryset.filter(home_type__iexact=value)
+            elif field == 'bathrooms':
+                bathrooms = float(value.rstrip('+'))
+                queryset = queryset.filter(bathrooms__gte=bathrooms)
+            elif field == 'sqft':
+                sqft = self.convert_sqft(value)
+                if sqft is not None:
+                    queryset = queryset.filter(sqft__gte=sqft)
+            elif field == 'days_listed':
+                days_passed = self.convert_days_listed(value)
+                if days_passed is not None:
+                    queryset = queryset.filter(list_date__gte=timezone.now() - timezone.timedelta(days=days_passed))
+            elif field == 'has_photos':
+                has_photos = int(value.rstrip('+'))
+                queryset = queryset.annotate(
+                    photo_count=(
+                        Q(photo_1__isnull=False) +
+                        Q(photo_2__isnull=False) +
+                        Q(photo_3__isnull=False) +
+                        Q(photo_4__isnull=False) +
+                        Q(photo_5__isnull=False) +
+                        Q(photo_6__isnull=False) +
+                        Q(photo_7__isnull=False) +
+                        Q(photo_8__isnull=False) +
+                        Q(photo_9__isnull=False) +
+                        Q(photo_10__isnull=False) +
+                        Q(photo_11__isnull=False) +
+                        Q(photo_12__isnull=False) +
+                        Q(photo_13__isnull=False) +
+                        Q(photo_14__isnull=False) +
+                        Q(photo_15__isnull=False) +
+                        Q(photo_16__isnull=False) +
+                        Q(photo_17__isnull=False) +
+                        Q(photo_18__isnull=False) +
+                        Q(photo_19__isnull=False) +
+                        Q(photo_20__isnull=False)
+                    )
+                )
+                queryset = queryset.filter(photo_count__gte=has_photos)
+            elif field == 'open_house':
+                open_house = self.convert_open_house(value)
+                if open_house is not None:
+                    queryset = queryset.filter(open_house__iexact=open_house)
+            elif field == 'keywords':
+                queryset = queryset.filter(desc__icontains=value)
 
         serializer = ListingSerializer(queryset, many=True)
-
         return Response(serializer.data)
+
+    def convert_price_range(self, price_str):
+        if price_str == 'Any':
+            return None
+        elif price_str.endswith('+'):
+            return int(price_str.replace('+', '').replace(',', ''))
+        else:
+            return None
+
+    def convert_sqft(self, sqft_str):
+        if sqft_str == 'Any':
+            return None
+        elif sqft_str.endswith('+'):
+            return int(sqft_str.replace('+', '').replace(',', ''))
+        else:
+            return None
+
+    def convert_days_listed(self, days_listed_str):
+        if days_listed_str == 'Any':
+            return None
+        elif ' or less' in days_listed_str:
+            return int(days_listed_str.split(' ')[0])
+        else:
+            return None
+
+    def convert_open_house(self, open_house_str):
+        if open_house_str.lower() in ['true', 'false']:
+            return open_house_str.lower()
+        else:
+            return None
